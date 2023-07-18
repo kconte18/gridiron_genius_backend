08372d8e98ced3a9bb50191dee556620de2f30eb
kconte18 <kconte810@gmail.com>
2023-07-18 14:40:27 -0500

    Added models and migrations

--- ranking_app/models.py
+++ ranking_app/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
 # Create your models here.
+POSITIONS = (
+    ('QB', 'QuarterBack'),
+    ('RB', 'RunningBack'),
+    ('WR', 'WideReceiver'),
+    ('TE', 'TightEnd'),
+    ('DEF', 'Defense'),
+    ('K', 'Kicker'),
+)
+
+SCORING_TYPES = (
+    ('STANDARD', 'STANDARD'),
+    ('HALF_PPR', 'HALF_PPR'),
+    ('PPR', 'PPR')
+)
+
+TEAMS = (
+    ('ARZ', 'Arizona Cardinals'),
+    ('ATL', 'Atlanta Falcons'),
+    ('BAL', 'Baltimore Ravens'),
+    ('BUF', 'Buffalo Bills'),
+    ('CAR', 'Carolina Panthers'),
+    ('CHI', 'Chicago Bears'),
+    ('CIN', 'Cincinnati Bengals'),
+    ('CLE', 'Cleveland Browns'),
+    ('DAL', 'Dallas Cowboys'),
+    ('DEN', 'Denver Broncos'),
+    ('DET', 'Detroit Lions'),
+    ('GB', 'Green Bay Packers'),
+    ('HOU', 'Houson Texans'),
+    ('IND', 'Indianapolis Colts'),
+    ('JAX', 'Jacksonville Jaguars'),
+    ('KC', 'Kansas City Chiefs'),
+    ('LV', 'Las Vegas Raiders'),
+    ('LAC', 'Los Angeles Chargers'),
+    ('LAR', 'Los Angeles Rams'),
+    ('MIA', 'Miami Dolphins'),
+    ('MIN', 'Minnesota Vikings'),
+    ('NE', 'New England Patriots'),
+    ('NO', 'New Orleans Saints'),
+    ('NYG', 'New York Giants'),
+    ('NYJ', 'New York Jets'),
+    ('PHI', 'Philadelphia Eagles'),
+    ('PIT', 'Pittsburgh Steelers'),
+    ('SF', 'San Francisco 49ers'),
+    ('SEA', 'Seattle Seahawks'),
+    ('TB', 'Tampa Bay Buccaneers'),
+    ('TEN', 'Tennessee Titans'),
+    ('WAS', 'Washington Commanders')
+)
+
+class Player(models.Model):
+    player_name = models.CharField(max_length=30)
+    position = models.CharField(max_length=3, choices=POSITIONS)
+    team = models.CharField(max_length=3, choices=TEAMS)
+    bye_week = models.IntegerField()
+
+class RankingSource(models.Model):
+    ranking_src_name = models.CharField(max_length=30)
+    scoring_type = models.CharField(max_length=10, choices=SCORING_TYPES)
+    players_count = models.IntegerField()
+
+class Ranking(models.Model):
+    ranking_src = models.ForeignKey(RankingSource, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    rank = models.IntegerField()
