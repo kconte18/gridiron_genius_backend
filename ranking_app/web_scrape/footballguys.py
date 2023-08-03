e67764e2abe97d44ed2ec976e35549aaa34dae0d
kconte18 <kconte810@gmail.com>
2023-08-02 21:45:12 -0500

    Updated football guys

--- ranking_app/web_scrape/footballguys.py
+++ ranking_app/web_scrape/footballguys.py
@@ -8,7 +8,7 @@ sources = rankings_sources.footballguys_sources
 def web_scrape(players_dict):
     for source in sources:
         rb_df = pd.read_html(source['url'])
-        raw_df = rb_df[0].iloc[lambda x: x.index < 113]
+        raw_df = rb_df[0].iloc[lambda x: x.index < 112]
         column_list = raw_df.columns.tolist()
         
         rank_column_value = column_list[0]
@@ -19,6 +19,7 @@ def web_scrape(players_dict):
         df = df[df["Rank"].str.contains("Tier") == False]
         df['Player'] = df['Player'].str.rstrip(' ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
         df = df.reindex(columns=['Player', 'Rank'])
+        print(df)
         
         source['df_list'] = helpers.swap_name_with_id(df, players_dict)
     return sources
